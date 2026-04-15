8da5aa7ba9585c6212c08dfcebe5992d4e0ab325
rba0606coruss <rafael@coruss.com.br>
2026-04-15 10:43:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'kadosh-cache-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME));
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
